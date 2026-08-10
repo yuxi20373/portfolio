8158d9007f84d079ffd528e4e6247b827b1bead5
yuxi <yuxi20373@gmail.com>
2026-08-10 21:00:23 +0800

    update

--- frontend/assets/js/avatars.js
+++ frontend/assets/js/avatars.js
@@ -3,16 +3,7 @@
 // 圖片。跟首頁那套會隨 image theme 切換的裝飾圖片不一樣 - 頭貼代表的是
 // 「這個人是誰」,跟目前選的視覺主題無關,所以固定同一份,路徑不走
 // store.img()(那個會依 store.imageTheme 換資料夾)。
-export const AVATAR_PRESETS = [
-  "avatar-1.png",
-  "avatar-2.png",
-  "avatar-3.png",
-  "avatar-4.png",
-  "avatar-5.png",
-  "avatar-6.png",
-  "avatar-7.png",
-  "avatar-8.png",
-];
+export const AVATAR_PRESETS = Array.from({ length: 20 }, (_, i) => `avatar-${i + 1}.png`);
 
 // avatar 是存在 User.avatar 的 preset 檔名(或 null/未設定)。回傳空字串時
 // 呼叫端應該顯示 fallback SVG(見 icons.js 的 icons.user),不要塞一個
